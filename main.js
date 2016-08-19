9c93f7524f3b32ea3322bc3d8f81df92a8353bad
gokulkrishh <krishnangokul9@gmail.com>
2016-08-19 13:10:11 +0530

    Added css position, transform example

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var menuIconEle = document.querySelector(".menu-icon");
 var menuEle = document.querySelector(".menu");
+var menuParentEle = document.querySelector(".menu-container");
 var menuOverlayEle = document.querySelector(".menu-overlay");
+var radioBtn = document.querySelectorAll("input");
 
 menuIconEle.addEventListener("click", function () {
   menuEle.className = "menu show";
@@ -11,3 +13,18 @@ menuOverlayEle.addEventListener("click", function () {
   menuEle.className = "menu";
   menuOverlayEle.className = "menu-overlay";
 });
+
+for (var i = 0; i < radioBtn.length; i++) {
+  radioBtn[i].addEventListener("click", handleClick)
+}
+
+function handleClick (event){
+  if (event.target.value === "transform") {
+    console.log("transform");
+    menuParentEle.className = "menu-container transform-property";
+  }
+  else {
+    console.log("position");
+    menuParentEle.className = "menu-container position-property";
+  }
+}
